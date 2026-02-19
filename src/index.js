f9ffb0bebdcd4df1ae50ebc3186199738f298068
Rudresh tiwari <rudraprataptiwari786@gmail.com>
2026-02-19 12:51:35 +0530

    fix: remove ngrok patches and fix import order in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App';
-
-// Patch global fetch to add ngrok bypass header for all requests
-const _originalFetch = window.fetch;
-window.fetch = (url, options = {}) => {
-  const headers = new Headers(options.headers || {});
-  headers.set('ngrok-skip-browser-warning', 'true');
-  return _originalFetch(url, { ...options, headers });
-};
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,6 @@ const api = axios.create({
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
-    'ngrok-skip-browser-warning': 'true',
   },
 });
 
